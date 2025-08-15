ba45745eaea5d1d08132b8348e5a0c402863d902
theonerishi <135510283+theonerishi@users.noreply.github.com>
2025-08-15 23:03:48 +0000

    completed two more exercises and removed .skip to enable tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    return arr.filter(item => !elements.includes(item));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    let total = 0;
+    if (n1 < 0 || n2 < 0 || typeof(n1) != 'number' || typeof(n2) != 'number' || n1 != Math.floor(n1) || n2 != Math.floor(n2)) {
+        return 'ERROR';
+    }
+    if (n1 > n2) {
+        let tmp = n1;
+        n1 = n2;
+        n2 = tmp;
+    }
+    for (let i = n1; i <= n2; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
