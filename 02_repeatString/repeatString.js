baaa036d8a5d994da61b19e9e6918e394ef80fde
theonerishi <135510283+theonerishi@users.noreply.github.com>
2025-08-15 22:01:41 +0000

    first 3 exercises complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n < 0) {
+        return 'ERROR';
+    }
+    let str2 = '';
+    for (let i = 0; i < n; i++) {
+        str2 += str;
+    }
+    return str2;
 };
 
 // Do not edit below this line
