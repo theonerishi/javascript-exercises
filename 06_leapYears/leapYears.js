4389904be14390057b8044cd6388d8bb3bfb0d42
theonerishi <135510283+theonerishi@users.noreply.github.com>
2025-08-16 11:05:47 +0000

    completed arrays and loops exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(n) {
+    if (n % 4 == 0) {
+        if (n % 100 == 0) {
+            if (n % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  n = (n - 32) * 5 / 9;
+  return Math.round(n * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(n) {
+  n = n * 9 / 5 + 32;
+  return Math.round(n * 10) / 10;
 };
 
 // Do not edit below this line
