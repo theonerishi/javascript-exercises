30c769be29b007ebd940a4918b76188c1c9104d7
theonerishi <135510283+theonerishi@users.noreply.github.com>
2026-07-08 13:34:37 +0000

    pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,17 +1,19 @@
 const pascal = function(n) {
     let arr = [];
-    let arr2 = [];
-    for (let row = 0; row < n; row++) {
-        let arr2 = [];
+    
+    for (let row = 1; row <= n; row++) {
+        let currentRow = [];
         for (let i = 0; i < row; i++) {
-            if (i == 0 || i == row) {
-                arr2.push(1);
+            if (i === 0 || i === row - 1) {
+                currentRow.push(1);
             } else {
-                arr2.push(arr[row - 1][i-1] + arr[row - 1][i]);
+                currentRow.push(arr[row - 2][i - 1] + arr[row - 2][i]);
             }
-        } 
+        }
+        arr.push(currentRow);
     }
+    
+    return arr[n - 1];
 };
-  
 // Do not edit below this line
 module.exports = pascal;
